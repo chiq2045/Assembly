b4a29aa4ac92ad55db73889315ed36709075ff6d
chiq2045 <charles.oroko@outlook.com>
2018-11-09 15:08:12 -0800

    Final corrections for chapter 4

--- Chapter 4/4.5.s
+++ Chapter 4/4.5.s
@@ -5,14 +5,9 @@ num2:   .word -34
         .text
         .global _start
 _start:
-ldr     r1, =num1
-ldr     r2, =num2
-ldr     r0, [r1]    @assume x>=y
-ldr     r2, [r2]
-cmp     r0, r2      @change r0=y if x<y
-
-bge     done
-mov     r0, r2
-
-done:
-add     r1, r1, #0
+    ldr     r1, =num1
+    ldr     r2, =num2
+    ldr     r0, [r1]
+    ldr     r2, [r2]
+    cmp     r0, r2
+    movle   r0, r2
--- Chapter 4/4.6.s
+++ Chapter 4/4.6.s
@@ -5,18 +5,6 @@ _start:
 mov     r0, #5      @r0=a
 mov     r1, #10     @r0=b
 
-@if (a&1)
-and     r2, r0, #1  
-cmp     r2, #0
-
-@a=-a
-beq     else
-equal:
-rsb     r0, r0, #0
-b       end
-
-@else b=b+7
-else:
-add     r1, r1, #7  
-
-end:
+tst     r0, #1      @if(a&1)
+rsbne   r0, r0, #0  @a=-a
+addeq   r1, r1, #7  @else b+=7
